5752faa9bae0b29f98cb416801cb4d425b6b11ce
Tework123 <daniliesaec@gmail.com>
2023-05-19 06:18:51 +0300

    host to server 2

--- app/__init__.py
+++ app/__init__.py
@@ -2,81 +2,73 @@ import os
 from flask_login import LoginManager
 from flask_mail import Mail
 from flask_sqlalchemy import SQLAlchemy
-from flask import Flask, redirect, url_for
+from flask import Flask
 from flask_migrate import Migrate
 from config import Config
 import logging
 from logging.handlers import SMTPHandler, RotatingFileHandler
 
-# создание объектов приложения и базы данных
-app = Flask(__name__)
-app.config.from_object(__name__)
-
-app.config['SQLALCHEMY_DATABASE_URI'] = Config.SQLALCHEMY_DATABASE_URI
-app.config['SECRET_KEY'] = Config.SECRET_KEY
-db = SQLAlchemy(app)
-migrate = Migrate(app, db)
-login_manager = LoginManager(app)
-
-# config for mail.send(msg)
-app.config['MAIL_SERVER'] = Config.MAIL_SERVER
-app.config['MAIL_PORT'] = int(Config.MAIL_PORT)
-app.config['MAIL_USE_TLS'] = Config.MAIL_USE_TLS
-app.config['MAIL_USERNAME'] = Config.MAIL_USERNAME
-app.config['MAIL_PASSWORD'] = Config.MAIL_PASSWORD
-
-mail = Mail(app)
-
-# config for SMTP logging
-mail_handler = SMTPHandler(mailhost=(Config.MAIL_SERVER, int(Config.MAIL_PORT)),
-                           fromaddr=Config.MAIL_USERNAME,
-                           toaddrs=Config.ADMINS,
-                           subject='Error flask_test_drive',
-                           credentials=(Config.MAIL_USERNAME, Config.MAIL_PASSWORD),
-                           secure=()
-                           )
-
-# for SMTP logging
-if not os.path.exists('logs'):
-    os.mkdir('logs')
-file_handler = RotatingFileHandler('logs/log_test.log', maxBytes=10240, backupCount=10)
-file_handler.setFormatter(logging.Formatter(
-    '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
-
-# action to logs/log.log
-file_handler.setLevel(logging.DEBUG)
-app.logger.addHandler(file_handler)
-
-# action to email
-mail_handler.setLevel(logging.ERROR)
-app.logger.addHandler(mail_handler)
-
-app.logger.setLevel(logging.INFO)
-app.logger.info('Start application')
-
-# импорт и регистрация blueprint
-from app.user import bp as bp_user
-
-app.register_blueprint(bp_user, url_prefix='/user')
-
-from app.admin import bp as bp_admin
-
-app.register_blueprint(bp_admin, url_prefix='/admin')
-
-from app.errors import bp as bp_errors
-
-app.register_blueprint(bp_errors, url_prefix='/errors')
-
-with app.app_context():
-    # db.drop_all()
-    # db.create_all()
-    pass
+db = SQLAlchemy()
+migrate = Migrate()
+login_manager = LoginManager()
+mail = Mail()
 
 
-# menu = [['Home', '/user'], ['Сar brands', '/user/show_brands'], ['Sing in', '/user/login'],
-#         ['Registration', '/user/register']]
+def create_app(config_class=Config):
+    # создание объектов приложения и базы данных
+    app = Flask(__name__)
 
+    # берет все конфиги из класса Config и заносит в приложение flask
+    app.config.from_object(config_class)
 
-@app.route('/')
-def index():
-    return redirect(url_for('user.index'))
+    # подключаем все модули
+    db.init_app(app)
+    migrate.init_app(app)
+    mail.init_app(app)
+    login_manager.init_app(app)
+
+    mail_handler = SMTPHandler(mailhost=(Config.MAIL_SERVER, int(Config.MAIL_PORT)),
+                               fromaddr=Config.MAIL_USERNAME,
+                               toaddrs=Config.ADMINS,
+                               subject='Error flask_test_drive',
+                               credentials=(Config.MAIL_USERNAME, Config.MAIL_PASSWORD),
+                               secure=()
+                               )
+
+    # for SMTP logging
+    if not os.path.exists('logs'):
+        os.mkdir('logs')
+    file_handler = RotatingFileHandler('logs/log_test.log', maxBytes=10240, backupCount=10)
+    file_handler.setFormatter(logging.Formatter(
+        '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
+
+    # action to logs/log.log
+    file_handler.setLevel(logging.DEBUG)
+    app.logger.addHandler(file_handler)
+
+    # action to email
+    mail_handler.setLevel(logging.ERROR)
+    app.logger.addHandler(mail_handler)
+
+    app.logger.setLevel(logging.INFO)
+    app.logger.info('Start application')
+
+    # импорт и регистрация blueprint
+    from app.user import bp as bp_user
+
+    app.register_blueprint(bp_user, url_prefix='/user')
+
+    from app.admin import bp as bp_admin
+
+    app.register_blueprint(bp_admin, url_prefix='/admin')
+
+    from app.errors import bp as bp_errors
+
+    app.register_blueprint(bp_errors, url_prefix='/errors')
+
+    with app.app_context():
+        # db.drop_all()
+        # db.create_all()
+        pass
+
+    return app
--- app/user/email.py
+++ app/user/email.py
@@ -1,7 +1,7 @@
-from flask import render_template
+from flask import render_template, current_app
 from flask_mail import Message
 
-from app import mail, app
+from app import mail
 from app.models import ResetPasswordStatic
 from config import Config
 
@@ -16,7 +16,7 @@ def send_email(subject, sender, recipients, body, attachments=None):
 
     if attachments is not None:
         attach_photo = attachments.split('/')[-1]
-        with app.open_resource(attachments) as fp:
+        with current_app.open_resource(attachments) as fp:
             msg.attach(attach_photo, 'image/png', fp.read())
     mail.send(msg)
 
@@ -25,7 +25,5 @@ def send_email(subject, sender, recipients, body, attachments=None):
 
 def send_password_reset_email(user):
     token = ResetPasswordStatic.get_reset_password_token(user)
-    print(token)
-
     body = render_template('user/reset_password_email.html', user=user, token=token)
     send_email('Reset_password', Config.MAIL_USERNAME, [user.email], body)
