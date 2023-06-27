0588be303dcc5e512db3b6cfb408e4e712dcd951
Tework123 <daniliesaec@gmail.com>
2023-06-27 16:22:43 +0300

    nginx

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(tework123.ru www.tework123.ru)
+domains=(tework123.ru)
 rsa_key_size=4096
 data_path="./data/certbot"
 email="daniliesaec@gmail.com" # Adding a valid address is strongly recommended
