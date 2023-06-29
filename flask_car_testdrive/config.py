3dbb9798c896bdbe9080636c68eebba4e89a5aaa
Tework123 <daniliesaec@gmail.com>
2023-06-29 08:06:15 +0300

    clear

--- flask_car_testdrive/config.py
+++ flask_car_testdrive/config.py
@@ -2,7 +2,8 @@ import os.path
 from dotenv import load_dotenv
 
 basedir = os.path.abspath(os.path.dirname(__file__))
-load_dotenv(os.path.join(basedir, '../.env'))
+basedir = '/'.join(basedir.split('/')[:-1])
+load_dotenv(os.path.join(basedir, './.env'))
 
 
 class Config(object):
