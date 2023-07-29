4c93c02a32779dbb3f6dd7b78c8a8a493865c470
Tework123 <daniliesaec@gmail.com>
2023-07-29 09:17:59 +0300

    without microservises

--- flask_car_testdrive/flask_car_testdrive.py
+++ flask_car_testdrive/flask_car_testdrive.py
@@ -4,14 +4,13 @@ from config import ProductionConfig
 from app import create_app
 
 # here change config
-CONFIG = ProductionConfig
+CONFIG = DevelopmentConfig
 
 # test server
 # CONFIG_TEST = 'https://tework123.ru/'
 
 # test with new_app on localhost
 CONFIG_TEST = 'new_app'
-
 
 app = create_app(CONFIG)
 
